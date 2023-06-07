85475fd9a145a0d2310bbd4deebd3e3c351a93cb
Amir Abdullaev <amabdullaev@yandex-team.ru>
2023-06-08 00:54:03 +0300

    Init setup

---
+++ personal/diff-hl-custom.el
@@ -0,0 +1,5 @@
+(setq diff-hl-margin-symbols-alist
+  '((insert . " ") (delete . " ") (change . " ")))
+
+(diff-hl-margin-mode t)
+
---
+++ personal/preload/ui.el
@@ -0,0 +1,10 @@
+(setq initial-frame-alist '((top . 0) (left . 0) (width . 220) (height . 64)))
+
+(setq prelude-theme 'nord)
+
+(set-frame-font "JetBrains Mono-12" nil t)
+
+(scroll-bar-mode -1)
+
+(mac-auto-operator-composition-mode t)
+
