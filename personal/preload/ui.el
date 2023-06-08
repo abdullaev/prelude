8635e54400b37d8df70bf7b19753f8bf8871b522
Amir Abdullaev <amabdullaev@yandex-team.ru>
2023-06-08 21:18:15 +0300

    Adjust frame

--- personal/preload/ui.el
+++ personal/preload/ui.el
@@ -1,4 +1,4 @@
-(setq initial-frame-alist '((top . 0) (left . 0) (width . 220) (height . 64)))
+(setq initial-frame-alist '((top . 36) (left . 118) (width . 220) (height . 60)))
 
 (setq prelude-theme 'nord)
 
@@ -7,4 +7,3 @@
 (scroll-bar-mode -1)
 
 (mac-auto-operator-composition-mode t)
-
