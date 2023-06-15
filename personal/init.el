9617564a3a5bde91a8b6bd4777241c5fac6928f8
Amir Abdullaev <amabdullaev@yandex-team.ru>
2023-06-15 23:06:59 +0300

    Fix

--- personal/init.el
+++ personal/init.el
@@ -10,7 +10,21 @@
 (require 'flycheck-clj-kondo)
 
 (setq redisplay-dont-pause t
-  scroll-margin 4
+  scroll-margin 5
   scroll-step 1
   scroll-conservatively 10
   scroll-preserve-screen-position 1)
+
+(setq company-minimum-prefix-length 1)
+(setq company-idle-delay 0.05)
+(setq company-show-numbers nil)
+(setq company-tooltip-flip-when-above nil)
+(setq company-tooltip-limit 10)
+(setq company-require-match 'never)
+(setq company-format-margin-function nil)
+(setq company-tooltip-minimum-width 56)
+(setq company-auto-commit nil)
+(setq company-box-doc-enable nil)
+(setq company-box-show-single-candidate 'never)
+
+(add-hook 'company-mode-hook 'company-box-mode)
